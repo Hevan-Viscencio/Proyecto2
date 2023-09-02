fc29f30df3f80c3bb1884aedd7ce872c4c2773be
Hevan-Viscencio <143463643+Hevan-Viscencio@users.noreply.github.com>
2023-09-02 01:04:39 +0000

    Clase del Viernes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <Mascota.hpp>
 
-int main(int argc, char const *argv[]){
+int main(int argc, char const *argv[])
+{
 
-    std::cout<<"Mensaje"<<std::endl;
+    std::cout << "Juego de Mascotas" << std::endl;
+    
+    //se crean 3 instancias
+    Mascota m1,m2,m3;
+    m1.EstablecerNombre("Axel");
+    m2.EstablecerNombre("Rocky");
+    m3.EstablecerNombre("Solovino");        
+
+
+    //Hay de salida, Entrada, Ambas
+
+
+    m1.DecirNombre();
+    m2.DecirNombre();
+    m3.DecirNombre();
+
+
+
+    return 0;
 }
